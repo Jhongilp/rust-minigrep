c796e3aae82a15399f7f2338f326e8e35089c84b
Jonathan Gil <jhongilp@gmail.com>
2020-06-27 13:50:37 -0500

    Add a better error handling when not enough arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     println!("Hello, world!");
@@ -7,7 +8,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.filename);
 
@@ -23,9 +28,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        
+        Ok(Config { query, filename })
     }
 }
