326f4466b3057d401d0b28a64b4cb1e0c706ddad
Jonathan Gil <jhongilp@gmail.com>
2020-06-27 18:11:32 -0500

    move new and run logic into his own module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+mod minigrep;
 
 fn main() {
     println!("Hello, world!");
@@ -9,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Aplication error: {}", e);
         process::exit(1);
     }
@@ -22,31 +21,3 @@ fn main() {
     
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for: {}", config.query);
-    println!("In file: {}", config.filename);
-
-    let contens =
-        fs::read_to_string(config.filename)?;
-
-    println!("With text:\n {}", contens);
-
-    Ok(())
-}
